7baad5ccbc48f1d5c2cd3ec3421d738ff61aea20
Stephen Holsapple <sholsapp@gmail.com>
2016-02-04 19:49:56 -0500

    Hate hate hate hate

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-puppet --version || exit 1
-puppet module install garethr-docker --modulepath ./modules || exit 1
-puppet module install puppetlabs-vcsrepo --modulepath ./modules || exit 1
-puppet module install stankevich-python --modulepath ./modules || exit 1
-puppet apply --modulepath ./modules manifests/site.pp || exit 1
+PUPPET=$(which puppet)
+$PUPPET --version || exit 1
+$PUPPET module install garethr-docker --modulepath ./modules || exit 1
+$PUPPET module install puppetlabs-vcsrepo --modulepath ./modules || exit 1
+$PUPPET module install stankevich-python --modulepath ./modules || exit 1
+$PUPPET apply --modulepath ./modules manifests/site.pp || exit 1

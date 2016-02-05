869ed3878571fb4ca1ba7e1baacf6efccf40e86d
Stephen Holsapple <sholsapp@gmail.com>
2016-02-04 19:09:14 -0500

    Print puppet version when run.
    This is to debug travis build failures.

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+puppet --version || exit 1
 puppet module install garethr-docker --modulepath ./modules || exit 1
 puppet module install puppetlabs-vcsrepo --modulepath ./modules || exit 1
 puppet module install stankevich-python --modulepath ./modules || exit 1

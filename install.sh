72478336e918385d8d6332c89bd8fe7e9b89ed27
ubuntu <ubuntu@debian>
2016-02-27 04:42:59 -0800

    Add command to install.sh to install docker.

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+puppet module install garethr-docker --modulepath ./modules
 puppet module install puppetlabs-vcsrepo --modulepath ./modules
 puppet module install stankevich-python --modulepath ./modules
 puppet apply --modulepath ./modules manifests/site.pp
